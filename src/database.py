e120ddb06c49e70d593bb9a6f2cefe297f12f01c
LoylP <nhphuc183@gmail.com>
2024-12-30 18:14:36 +0000

    add function 'add data for csv file'

--- src/database.py
+++ src/database.py
@@ -11,6 +11,18 @@ def get_qa_data():
     engine = create_engine('sqlite:///qa_database.db')
     return pd.read_sql_table('qa_table', engine)
 
+def add_data_to_database(path_data):
+    engine = create_engine('sqlite:///qa_database.db')
+    df_new = pd.read_csv(path_data)
+
+    # Thêm dữ liệu vào bảng, đảm bảo không trùng lặp
+    existing_data = pd.read_sql_table('qa_table', engine)
+    combined_data = pd.concat([existing_data, df_new]).drop_duplicates()
+
+    combined_data.to_sql('qa_table', con=engine, if_exists='replace', index=False)
+
 # init_database()
-# df = get_qa_data()
-# print(df)
+add_data_to_database("/home/loylp/project/UIT-InfoBot/data/data.csv")
+
+df = get_qa_data()
+print(df)
